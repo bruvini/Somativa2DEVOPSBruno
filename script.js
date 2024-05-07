095c07f59d5456d26186c4d34d8287d94cb255c9
Bruno Vinícius da Silva <bruvini.silva12@gmail.com>
2024-05-06 23:16:46 -0300

    Primeiro Teste da Pipeline

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.getElementById('messageButton').addEventListener('click', function() {
         "Mantenha a positividade!",
         "Você está fazendo um ótimo trabalho!",
         "Continue assim!",
-        "A persistência é o caminho do êxito."
+        "A persistência é o caminho do êxito.",
+        "Ser ou não ser, eis a questão"
     ];
     const randomIndex = Math.floor(Math.random() * messages.length);
     document.getElementById('message').textContent = messages[randomIndex];
