ede60808282904311e19d44ea55c67d9ddfd3deb
Bruno Vinícius da Silva <bruvini.silva12@gmail.com>
2024-05-06 23:30:17 -0300

    Atualização 3

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ document.getElementById('messageButton').addEventListener('click', function() {
         "Continue assim!",
         "A persistência é o caminho do êxito.",
         "Ser ou não ser, eis a questão",
-        "Vivendo e aprendendo"
+        "Vivendo e aprendendo",
+        "Nem tudo que reluz é ouro!"
     ];
     const randomIndex = Math.floor(Math.random() * messages.length);
     document.getElementById('message').textContent = messages[randomIndex];
